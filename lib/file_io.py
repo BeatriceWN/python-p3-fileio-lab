d37326a8756736518cf141e015f535832412902d
Beatrice Wambui <bettyneema2@gmail.com>
2025-09-28 15:08:13 +0300

    commits fileio lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,43 @@
 def write_file(file_name, file_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    
+    with open(full_file_name, 'w') as file:
+        file.write(file_content)
+    
+    print(f"Content written to {full_file_name}")
+
 
 def append_file(file_name, append_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    
+    with open(full_file_name, 'a') as file:
+        file.write(append_content) 
+    
+    print(f"Content appended to {full_file_name}")
 
 def read_file(file_name):
-    pass
+    full_file_name = f"{file_name}.txt"
+    
+    try:
+        with open(full_file_name, 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        print(f"Error: {full_file_name} does not exist.")
+        return None
+
+
+
+
+from lib.file_io import write_file, append_file, read_file
+
+#create a new log file
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added") #output: Log 1: 5 bananas added
+
+#append new logs
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted") #output: Log 2: 3 bananas subtracted
+append_file(file_name="logfile", append_content="Log 3: 10 bananas sold") #output: Log 3: 10 bananas sold
+
+#read the log file
+content = read_file(file_name="logfile")
+print(content)
